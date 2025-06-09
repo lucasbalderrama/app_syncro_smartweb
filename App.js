d1295c5a22754564baa2d31de3b9ec8e22e631d3
Ana-Livia-Lopes <ana.livia.santos.lopes@gmail.com>
2025-06-09 18:40:24 -0300

    aaaaaaaaaaaaa

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
 // Lucas Randal e Gabriel Reis
 import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
-import StackNavigation from "./src/routes/Routes";
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
+import Cadastro from './src/screens/Cadastro';
+import Perfil from './src/screens/Perfil';
+import Login from './src/screens/Login';
+import Chat from './src/screens/Chat';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <StackNavigation />
-    </NavigationContainer>
+        <View style={{ flex: 1 }}>
+      <NavigationContainer>
+        <Drawer.Navigator initialRouteName='chat'>
+          <Drawer.Screen name="Cadastro" component={Cadastro} />
+          <Drawer.Screen name="Perfil" component={Perfil} />
+          <Drawer.Screen name="Login" component={Login} />
+          <Drawer.Screen name="Chat" component={Chat} />
+        </Drawer.Navigator>
+      </NavigationContainer>
+    </View>
   );
 }
